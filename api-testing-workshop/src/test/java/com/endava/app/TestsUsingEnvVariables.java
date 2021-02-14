0bca090c811ca83c3aeec2fec9cc63b7bebbd843
scabrales <scabrales@mcclatchyservices.com>
2021-02-14 17:21:25 -0500

    Add better tests

--- api-testing-workshop/src/test/java/com/endava/app/TestsUsingEnvVariables.java
+++ api-testing-workshop/src/test/java/com/endava/app/TestsUsingEnvVariables.java
@@ -2,10 +2,13 @@ package com.endava.app;
 
 import com.endava.app.helpers.TestEnv;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 
 import static io.restassured.RestAssured.given;
@@ -44,4 +47,58 @@ public class TestsUsingEnvVariables {
 				assertThat().
 				statusCode(200);
 	}
+
+
+	/*
+	Just to test the status code isn't enough!!!!! Remember that API test is a big phase in our test automation journey
+	We can make any meaningful test that you can think of. We can assert fields in the response, assert response times
+	response details, among other tests.
+	 */
+
+	@Test
+	public void IdListOnlyHasPositiveIds(){
+
+		Properties props = new Properties();
+		try {
+			props.load(new FileInputStream("application.properties"));
+		} catch (IOException var2) {
+			System.out.println("Hay un error leyendo el archivo de properties");
+		}
+		boolean areItemsPositive=true;
+		Response response = given().contentType(ContentType.JSON).when().get(props.getProperty("url")+"/booking").andReturn();
+		List<Integer> listIds=response.then().extract().path("bookingid");
+
+		Assert.assertTrue("The list of ids is empty",listIds.size()>0);
+
+		for(Integer bookingId: listIds){
+			if(bookingId <=0){
+				areItemsPositive=false;
+				break;
+			}
+		}
+		Assert.assertTrue("An id is negative",areItemsPositive);
+	}
+
+	/*
+	Exercise: Lets use the /booking/1 endpoint to assert that the name fisrtname field is different than your name.
+	 */
+
+
+	@Test
+	public void TestFirstNameValueOfFirstBooking(){
+
+		Properties props = new Properties();
+		try {
+			props.load(new FileInputStream("application.properties"));
+		} catch (IOException var2) {
+			System.out.println("Hay un error leyendo el archivo de properties");
+		}
+		boolean areItemsPositive=true;
+		Response response = given().contentType(ContentType.JSON).when().get(props.getProperty("url")+"/booking/1").andReturn();
+		String name =response.then().extract().path("firstname");
+		System.out.println("The name is: "+ name);
+		Assert.assertTrue("We have an incorrect name",name!="Santiago");
+
+	}
+// Continues in feature/4-Separating-Logics
 }
